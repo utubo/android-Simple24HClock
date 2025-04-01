d2a1fb443321a06475615d01a69d2e40f0347b68
utubo <utbutb@gmail.com>
2025-04-01 22:28:42 +0900

    Add the widget to home screen when the app is opened

---
+++ app/src/main/java/utb/dip/jp/simple24hclock/MainActivity.kt
@@ -0,0 +1,20 @@
+package utb.dip.jp.simple24hclock
+
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Add the widget to home screen
+        val context = this.applicationContext
+        val appWidgetManager = AppWidgetManager.getInstance(context)
+        val myProvider = ComponentName(context, AppWidget::class.java)
+        if (appWidgetManager.isRequestPinAppWidgetSupported) {
+            appWidgetManager.requestPinAppWidget(myProvider, null, null)
+        }
+        finish()
+    }
+}
