3853572fc2b733b5d298998c8b22b7ccaf22547b
utubo <utbutb@gmail.com>
2025-04-05 13:46:02 +0900

    v1.0(7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "utb.dip.jp.simple24hclock"
         minSdk = 31
         targetSdk = 35
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
