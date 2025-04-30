c941ddd3738448d2d46d6b7a0297913e76057846
utubo <utbutb@gmail.com>
2025-04-30 22:57:48 +0900

    Refactoring for update the widget

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "utb.dip.jp.simple24hclock"
         minSdk = 31
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.1"
+        versionCode = 11
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/utb/dip/jp/simple24hclock/WidgetUpdateWorker.kt
+++ app/src/main/java/utb/dip/jp/simple24hclock/WidgetUpdateWorker.kt
@@ -5,6 +5,7 @@ import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.Intent
+import java.util.Calendar
 import java.util.Date
 
 fun createIntent(context: Context): PendingIntent {
@@ -18,19 +19,29 @@ fun createIntent(context: Context): PendingIntent {
 
 internal fun setupNext(context: Context) {
     val manager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-    val now = Date().time
-    val interval = 60_000L
-    val next = now + (interval - now % interval)
+    val next = Calendar.getInstance()
+    next.set(Calendar.MILLISECOND, 0)
+    next.set(Calendar.SECOND, 0)
+    next.add(Calendar.MINUTE, 1)
     val pendingIntent = createIntent(context)
     manager.cancel(pendingIntent)
     manager.set(
         AlarmManager.RTC,
-        next,
+        next.time.time,
         createIntent(context)
     )
 }
 
 internal fun restart(context: Context) {
+    // Debounce
+    val workerPrefs = context.getSharedPreferences(WIDGET_WORKER_KEY, Context.MODE_PRIVATE)
+    val lastTick = workerPrefs.getLong("last_restart_time", 0L)
+    val now = Date().time
+    if (lastTick != 0L && now - lastTick < 100) return
+    workerPrefs.edit().apply {
+        putLong("last_restart_time", now)
+        apply()
+    }
     updateAllAppWidgets(context, AppWidgetManager.getInstance(context))
     setupNext(context)
 }
