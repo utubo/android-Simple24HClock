55e5b227bcae776dca7458d85d8b9d94c3dbd53b
utubo <utbutb@gmail.com>
2026-04-08 07:49:33 +0900

    refactor

--- app/src/main/java/utb/dip/jp/simple24hclock/SettingsActivity.kt
+++ app/src/main/java/utb/dip/jp/simple24hclock/SettingsActivity.kt
@@ -9,14 +9,12 @@ import android.content.pm.PackageManager
 import android.content.res.Configuration
 import android.graphics.Color
 import android.graphics.Matrix
-import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.text.method.LinkMovementMethod
 import android.util.TypedValue
 import android.util.TypedValue.COMPLEX_UNIT_PX
 import android.view.MotionEvent
-import android.view.View.GONE
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RemoteViews
@@ -346,12 +344,8 @@ class SettingsActivity : FragmentActivity() {
             finish()
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            v.tvPreventTimeLag.setOnClickListener {
-                startActivity(Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM))
-            }
-        } else {
-            v.tvPreventTimeLag.visibility = GONE
+        v.tvPreventTimeLag.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM))
         }
 
         v.tvLicenses.setOnClickListener {
